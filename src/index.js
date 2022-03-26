cba22e3fb4e460452c93aa87428a42fb9372084b
Hete Caetano <hetecaetano@gmail.com>
2022-03-26 20:30:05 -0300

    Adicionando a rota post

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const protocol = process.env.PROTOCOL || "http";
 const ip = require('ip').address();
 const port = process.env.PORT || 3100
 
+const routes = require('./routes');
+app.use(routes)
+
 app.listen(port, ()=> console.log(`
     Servidor rodando na porta http://localhost:${port} ou ${protocol}://${ip}:${port}
 `))
